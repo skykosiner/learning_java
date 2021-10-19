bd160c6a12b46c676143e8cf0bce63e69e847bab
Yoni Kosiner <yoni@kosiner.co.uk>
2021-10-20 00:20:11 +0100

    java

--- hello_world/src/app.java
+++ hello_world/src/app.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, World!");
+        String Nice = math(6,9,6,9) + "\nNice";
+        System.out.println(Nice);
+    };
+
+    private static int math(int num1, int num2, int num3, int num4) {
+        return num1 * num2 + num3 + num4;
     };
 };
